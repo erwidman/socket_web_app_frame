7148443a5226b47a063aabb0175733ff9431f269
erwidman <erwidman@mtu.edu>
2017-09-17 13:13:38 -0400

    datahandler updates

--- server.js
+++ server.js
@@ -1,18 +1,73 @@
-var fs = require('fs');
-var express = require('express');
+
+
 var vm = require('vm');
-var http = require('http');
+
 var striptags = require('striptags');
 var validator = require('validator');
-const SocketManager = require('./socketManager.js');
 
+
+
+//____________________________________________________APP_INSTANTIATE
+var http = require('http');
+var express = require('express');
 var app = express();
 var server = http.createServer(app);
 app.use(express.static('public'));
 app.use(express.static('socket.io/socket.io.js'));
-
-//start file server and socket.io
 server.listen(4040);
+
+
+
+
+//____________________________________________________JSONRY/DATA
+const DataHandler = require('./dataHandler.js');
+var dHandler = new DataHandler(); 
+
+var stores = {
+  "store": {
+    "book": [ 
+      {
+        "category": "reference",
+        "author": "Nigel Rees",
+        "title": "Sayings of the Century",
+        "price": 8.95
+      }, {
+        "category": "fiction",
+        "author": "Evelyn Waugh",
+        "title": "Sword of Honour",
+        "price": 12.99
+      }, {
+        "category": "fiction",
+        "author": "Herman Melville",
+        "title": "Moby Dick",
+        "isbn": "0-553-21311-3",
+        "price": 8.99
+      }, {
+         "category": "fiction",
+        "author": "J. R. R. Tolkien",
+        "title": "The Lord of the Rings",
+        "isbn": "0-395-19395-8",
+        "price": 22.99
+      }
+    ],
+    "bicycle": {
+      "color": "red",
+      "price": 19.95
+    }
+  }
+};
+
+dHandler.saveObject('stores',stores);
+
+setTimeout(function(){
+	
+	dHandler.queryObject('stores','$..book[?(@.price<10)]',function(data){
+		console.log(data);
+	});
+},5000);
+
+//____________________________________________________SOCKET_SETUP
+const SocketManager = require('./socketManager.js');
 var manager = new SocketManager(server);
 
 
