d55d2ed0f7b099ebd7b013db676f258e1791fdef
erwidman <erwidman@mtu.edu>
2017-09-17 13:40:35 -0400

    reorganize

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var app = express();
 var server = http.createServer(app);
 app.use(express.static('public'));
 app.use(express.static('socket.io/socket.io.js'));
+app.use('/boot', express.static('/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 server.listen(4040);
 
 
